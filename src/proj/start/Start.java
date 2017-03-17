11beb688580713a26611ccdd1284ea77cbe096d8
edLinux <edtester@yandex.ru>
2017-03-17 23:35:57 +0500

    Initialize new repo

---
+++ src/proj/start/Start.java
@@ -0,0 +1,14 @@
+package proj.start;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jkdf");
+
+	}
+
+}
